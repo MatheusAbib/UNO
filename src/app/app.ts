fcbfd1bb6d5b1182576311423984abf41c8e7954
MATHEUS BILITARDO ABIB <97857@pmogi.local>
2026-07-13 08:45:17 -0300

    Corrige emojis e melhora responsividade

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrls: ['./app.css']
+  // styleUrls: ['./app.css']
 })
 export class AppComponent implements OnInit {
   title = 'uno-game';
